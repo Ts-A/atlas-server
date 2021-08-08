22411f298ff09bbaf477898be4b20c4f7ac098d7
T.S.Ajeet <codewithstatus200@gmail.com>
2021-08-08 17:51:01 +0530

    feat: pin CRD apis

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const { MONGODB_URI } = process.env;
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: true,
+  useFindAndModify: false,
 });
 
 mongoose.connection.on('connected', () => {
--- src/apis/pinApi.js
+++ src/apis/pinApi.js
@@ -1,3 +1,47 @@
 const router = require('express').Router();
+const Pin = require('../models/Pin/pinModel');
+
+router.get('/', async (req, res) => {
+  try {
+    const pins = await Pin.find({});
+    res.status(200).json({ pins });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get('/:pin_id', async (req, res) => {
+  try {
+    const pin = await Pin.findOne({ _id: req.params.pin_id });
+    if (!pin) throw new Error('Invalid Pin ID');
+    res.status(200).json({ pin });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const pin = new Pin({ ...req.body });
+    await pin.save();
+    res.status(200).json({ pin });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.delete('/:pin_id', async (req, res) => {
+  try {
+    const pin = await Pin.findOneAndRemove({ _id: req.params.pin_id });
+    if (!pin) throw new Error('Invalid Pin ID');
+    res.status(200).json({ pin });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ error: error.message });
+  }
+});
 
 module.exports = router;
