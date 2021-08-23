642b83b867fca288d6373ae1152ac5efa35d8fff
T.S.Ajeet <codewithstatus200@gmail.com>
2021-08-23 10:45:36 +0530

    fix: pin and user model updated

--- src/models/Pin/pinModel.js
+++ src/models/Pin/pinModel.js
@@ -2,23 +2,29 @@ const mongoose = require('mongoose');
 
 const PinSchema = mongoose.Schema(
   {
-    username: {
-      type: String,
-    },
     title: {
       type: String,
+      required: true,
     },
     description: {
       type: String,
+      required: true,
     },
     rating: {
       type: Number,
+      required: true,
     },
     latitude: {
       type: Number,
+      required: true,
     },
     longitude: {
       type: Number,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
   },
   { timestamps: true }
--- src/models/User/userModel.js
+++ src/models/User/userModel.js
@@ -4,13 +4,29 @@ const UserSchema = mongoose.Schema(
   {
     username: {
       type: String,
+      required: true,
+      unique: true,
     },
     email: {
       type: String,
+      required: true,
+      unique: true,
     },
     password: {
       type: String,
+      required: true,
     },
+    color: {
+      type: String,
+      required: false,
+      default: '#ffffff',
+    },
+    pins: [
+      {
+        ref: 'Pin',
+        type: mongoose.Schema.Types.ObjectId,
+      },
+    ],
   },
   { timestamps: true }
 );
